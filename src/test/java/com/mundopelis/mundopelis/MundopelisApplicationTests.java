8778205f8de0686c676880b7e6a198c4eda59516
gallegohector96 <gallegohector96@gmail.com>
2023-06-28 12:22:13 -0500

    Backen para el TCC de Topicos avanzados de software - simulación de una app de Peliculas - CRUD

---
+++ src/test/java/com/mundopelis/mundopelis/MundopelisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mundopelis.mundopelis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MundopelisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
